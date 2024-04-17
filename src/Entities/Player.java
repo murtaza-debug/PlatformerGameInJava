ebf8d3cbd2c3272e916ce017b0b8091e8698a764
murtaza-debug <murtazashah479@gmail.com>
2024-04-17 22:22:58 +0500

    Update Player.java

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -3,32 +3,25 @@ package Entities;
 import UserInput.Keyboard;
 
 import java.awt.*;
+import static MAINGAME.Panel.*;
 
 public class Player extends Entity{
 
 
     /// defaults ////
-
     private Keyboard keyboard ;
 
 
+    //// HIT_BOX //////
+    Rectangle hitBox;
+    Dimension dimensionHitBox ;
 
+    //////// CONSTRUCTOR /////////
     public Player ()
     {
+        dimensionHitBox = new Dimension(TILE_SIZE,TILE_SIZE);
         keyboard = new Keyboard() ;
-    }
-
-    @Override
-    public void update() {
-        updatePosition();
-    }
-
-    @Override
-    public void draw(Graphics g) {
-        g.setColor(Color.white);
-        g.fillRect(this.x,this.y , 64, 64);
-        g.setColor(Color.RED);
-        g.drawString("x :" + x + "  y : " + y , x , y - 10);
+        hitBox = new Rectangle(dimensionHitBox);
     }
 
 
@@ -42,6 +35,37 @@ public class Player extends Entity{
         if (keyboard.Right) this.x += speed;
     }
 
+    ///// UPDATE AND DRAW HIT_BOX  ///////
+
+    public void updateHitBox ()
+    {
+        hitBox.x = x;
+        hitBox.y = y;
+    }
+    public void drawHitBox (Graphics g)
+    {
+        g.setColor(Color.YELLOW);
+        g.drawRect(hitBox.x,hitBox.y,hitBox.width,hitBox.height);
+    }
+
+    ////// UPDATE AND DRAW /////
+
+    @Override
+    public void update() {
+        updatePosition();
+        updateHitBox();
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(this.x,this.y , 64, 64);
+        g.setColor(Color.RED);
+        g.drawString("x :" + x + "  y : " + y , x , y - 10);
+        drawHitBox(g);
+    }
+
+    //// GETTERS AND SETTERS ////////
     public Keyboard getKeyboard() {
         return this.keyboard;
     }
